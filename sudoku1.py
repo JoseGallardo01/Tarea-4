f5d67bb44382c97289be9526628be3ca9cc5543a
JoseGallardo01 <92793617+JoseGallardo01@users.noreply.github.com>
2021-11-05 16:23:01 -0300

    Adición de funciones.
    Se agregan 2 funciones, la primera llamada "resol" encargada de determinar si un número es apto, es decir, si no se repite en las filas, columnas y cuadrícula de 3x3. Si esto ocurre devuelve el valor lógico "false".
    La segunda función se llama Sudoku, se encarga de realizar la iteración y llama la función anterior para operar, con la técnica backtracking. Si se cumplen las condiciones, la función asigna a la casilla (fil,col) el valor de num y se ejecuta hasta rellenar todos los espacios de la tabla.

--- sudoku1.py
+++ sudoku1.py
@@ -15,3 +15,33 @@ tabla = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
     [0, 6, 0, 0, 0, 0, 2, 8, 0],
     [0, 0, 0, 4, 1, 9, 0, 0, 5],
     [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+def resol(cuadricula, fil, col, num):
+    for x in range(M):
+        if cuadricula[fil][x] == num:
+            return False
+    for x in range(M):
+        if cuadricula[x][col] == num:
+            return False
+    FilInicio = fil - fil % 3
+    ColInicio = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if cuadricula[i + FilInicio][j + ColInicio] == num:
+                return False
+    return True
+
+def suduko(cuadricula, fil, col):
+    if (fil == M - 1 and col == M):
+        return True
+    if col == M:
+        fil += 1
+        col = 0
+    if cuadricula[fil][col] > 0:
+        return suduko(cuadricula, fil, col + 1)
+    for num in range(1, M + 1, 1): 
+        if resol(cuadricula, fil, col, num):
+            cuadricula[fil][col] = num
+            if suduko(cuadricula, fil, col + 1):
+                return True
+        cuadricula[fil][col] = 0
+    return False
