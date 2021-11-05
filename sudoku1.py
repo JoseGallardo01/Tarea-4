cf6d827bae9a31336cafb01165e2cbc12af9ad66
JoseGallardo01 <92793617+JoseGallardo01@users.noreply.github.com>
2021-11-05 14:58:31 -0300

    Matriz de juego inicial
    Se agrega la matriz de Sudoku con las pistas dadas y la variable M, que corresponde a la dimensión de esta matriz.

---
+++ sudoku1.py
@@ -0,0 +1,17 @@
+'''
+Solucionador de Sudoku
+Creado por José Francisco Gallardo ojeda
+'''
+
+'''Se define la variable M, correspondiente a la cantidad de filas y columnas, es decir de la matríz MxM'''
+M = 9
+'''Se define la tabla con las pistas entregadas, agregando 0 en las casillas no definidas.'''
+tabla = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]]
